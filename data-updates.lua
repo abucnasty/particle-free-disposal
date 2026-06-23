76faea2ab1812e03657f1141733b03628ae9062b
abucnasty <abucnasty@gmail.com>
2026-06-23 15:16:58 -0400

    remove additional destroyed particles

--- data-updates.lua
+++ data-updates.lua
@@ -1,19 +1,74 @@
--- remove all damage effects from containers (steel-chest, iron-chest, etc.)
+-- ============================================================
+-- containers
+-- ============================================================
 for _, container in pairs(data.raw.container) do
   container.damaged_trigger_effect = nil
   container.dying_explosion = nil
 end
 
--- remove all damage effects from walls (steel-chest, iron-chest, etc.)
+-- ============================================================
+-- walls
+-- ============================================================
 for _, wall in pairs(data.raw.wall) do
   wall.damaged_trigger_effect = nil
   wall.dying_explosion = nil
 end
 
--- remove railgun turret shell particle
+-- ============================================================
+-- turrets
+-- ============================================================
 data.raw["ammo-turret"]["railgun-turret"].attack_parameters.shell_particle = nil
--- remove ammot turret shell particle
 data.raw["ammo-turret"]["gun-turret"].attack_parameters.shell_particle = nil
 
--- remove animation of big biter death
-data.raw["unit"]["big-biter"].dying_explosion = nil
+-- ============================================================
+-- units (biters / spitters)
+-- ============================================================
+for _, unit in pairs(data.raw.unit) do
+  if unit.name:find("biter") or unit.name:find("spitter") then
+    unit.damaged_trigger_effect = nil
+    unit.dying_explosion = nil
+  end
+end
+
+-- ============================================================
+-- explosions
+-- ============================================================
+
+-- strip specific create-particle effects from explosion chains
+local target_particles = {
+  ["cable-and-electronics-particle-small-medium"] = true,
+  ["car-metal-particle-small"] = true,
+  ["car-metal-particle-medium"] = true,
+  ["car-metal-particle-big"] = true,
+}
+
+local function strip_target_particle_effects(target_effects)
+  if type(target_effects) ~= "table" then return end
+  for i = #target_effects, 1, -1 do
+    local effect = target_effects[i]
+    if type(effect) == "table" and effect.type == "create-particle" and target_particles[effect.particle_name] then
+      table.remove(target_effects, i)
+    end
+  end
+end
+
+for _, explosion in pairs(data.raw.explosion) do
+  -- remove blood explosions entirely
+  if explosion.name:find("blood") then
+    explosion.created_effect = nil
+  end
+
+  -- strip targeted particles from all other explosions
+  local ce = explosion.created_effect
+  if type(ce) == "table" then
+    local actions = ce.type and {ce} or ce
+    for _, action in pairs(actions) do
+      if type(action.action_delivery) == "table" then
+        local deliveries = action.action_delivery.type and {action.action_delivery} or action.action_delivery
+        for _, delivery in pairs(deliveries) do
+          strip_target_particle_effects(delivery.target_effects)
+        end
+      end
+    end
+  end
+end
